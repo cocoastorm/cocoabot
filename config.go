0f5abdf3685d6d0b2ec1d8beb7e4e185012c51ac
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2019-02-08 19:52:07 -0800

    chore: add roles to config

--- config.go
+++ config.go
@@ -4,10 +4,12 @@ import "os"
 
 type Config struct {
 	BotToken string
+	Roles    []string
 }
 
 func initConfig(c *Config) {
 	c = &Config{
 		BotToken: os.Getenv("BOT_TOKEN"),
+		Roles:    []string{"musiclover"},
 	}
 }
