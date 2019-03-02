0f85260ada0b067da035d9fd459914f82b29b46b
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2019-03-02 12:02:46 -0800

    feat: allow additional roles with ALLOWED_ROLES env

--- config.go
+++ config.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"os"
+	"strings"
 
 	"google.golang.org/api/googleapi/transport"
 	"google.golang.org/api/youtube/v3"
@@ -21,9 +22,19 @@ func initConfig(c *Config) {
 
 	c.BotToken = os.Getenv("BOT_TOKEN")
 	c.YouTubeKey = os.Getenv("YOUTUBE_KEY")
+
+	// default allowed roles
 	c.Roles = []string{
 		"music",
 		"music lover",
+	}
+
+	// additional read in roles
+	input := strings.TrimSpace(os.Getenv("ALLOWED_ROLES"))
+	roles := strings.Split(input, ",")
+
+	for _, role := range roles {
+		c.Roles = append(c.Roles, role)
 	}
 }
 
