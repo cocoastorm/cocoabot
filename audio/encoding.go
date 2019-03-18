58d0b48d76bb08578ad2a7b35d6d0ad42e670d02
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2019-03-18 09:02:35 -0700

    doesn't work

--- audio/encoding.go
+++ audio/encoding.go
@@ -1,10 +1,12 @@
 package audio
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/binary"
+	"errors"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"os/exec"
 	"sync"
@@ -16,23 +18,27 @@ type Frame []byte
 
 type Encoding struct {
 	mu            sync.Mutex
-	options       AudioOptions
+	options       *AudioOptions
 	input         string
 	frameChannel  chan Frame
 	ffmpegProcess *os.Process
-	lastframe     int
 	running       bool
+	stop          chan bool
+	lastframe     int
 }
 
 func (e *Encoding) encodeArgs() []string {
-	args := e.options.FFmpegArgs()
-	args = append(args, []string{
+	args := []string{
 		"-i", e.input,
 		"-map", "0:a",
 		"-acodec", "libopus",
 		"-f", "ogg",
-		"-pipe:1",
-	}...)
+	}
+
+	encodeArgs := e.options.FFmpegArgs()
+
+	args = append(args, encodeArgs...)
+	args = append(args, "pipe:1")
 
 	return args
 }
@@ -40,12 +46,17 @@ func (e *Encoding) encodeArgs() []string {
 func (e *Encoding) start() {
 	e.mu.Lock()
 
+	// reset state at the end
 	defer func() {
 		e.running = false
 		e.mu.Unlock()
 	}()
 
-	run := exec.Command("ffmpeg", e.encodeArgs()...)
+	args := e.encodeArgs()
+
+	fmt.Println(args)
+
+	run := exec.Command("ffmpeg", args...)
 
 	stdout, err := run.StdoutPipe()
 	if err != nil {
@@ -53,46 +64,112 @@ func (e *Encoding) start() {
 		return
 	}
 
-	ffmpegbuf := bufio.NewReaderSize(stdout, 16384)
+	// debug stderr
+	stderr, err := run.StderrPipe()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	bufstderr := new(bytes.Buffer)
+
+	// ffmpegbuf := bufio.NewReaderSize(stdout, 16384)
 
 	err = run.Start()
 	defer run.Process.Kill()
+
+	e.mu.Unlock()
 
 	if err != nil {
 		fmt.Printf("ffmpeg failed to start: %s\n", err.Error())
 		return
 	}
 
-	decoder := ogg.NewPacketDecoder(ogg.NewDecoder(ffmpegbuf))
+	bufstderr.ReadFrom(stderr)
+	fmt.Println(bufstderr.String())
+
+	decoder := ogg.NewPacketDecoder(ogg.NewDecoder(stdout))
 	skip := 2
+
+	fmt.Println(skip)
 
 	for {
 		buf := new(bytes.Buffer)
 		packet, _, err := decoder.Decode()
 
+		fmt.Println(packet)
+
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+		// skip the ogg metadata packets
 		if skip > 0 {
+			fmt.Println("skip")
 			skip--
 			continue
 		}
 
-		if err != nil {
-			break
-		}
-
 		err = binary.Write(buf, binary.LittleEndian, int16(len(packet)))
 		if err != nil {
+			log.Println(err)
 			break
 		}
 
 		_, err = buf.Write(packet)
 		if err != nil {
+			log.Println(err)
 			break
 		}
 
 		e.frameChannel <- Frame(buf.Bytes())
 
-		e.mu.Lock()
-		e.lastframe++
-		e.mu.Unlock()
+		// select {
+		// case <-e.stop:
+		// 	break
+		// default:
+		// 	continue
+		// }
+
+		// e.mu.Lock()
+		// e.lastframe++
+		// e.mu.Unlock()
 	}
+
+	err = run.Wait()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+func Encode(input string, opts *AudioOptions) *Encoding {
+	e := &Encoding{
+		input:        input,
+		options:      opts,
+		frameChannel: make(chan Frame, opts.BufferedFrames),
+		stop:         make(chan bool),
+	}
+
+	go e.start()
+
+	return e
+}
+
+func (e *Encoding) OpusFrame() (frame []byte, err error) {
+	f := <-e.frameChannel
+	if f == nil {
+		return nil, io.EOF
+	}
+
+	if len(f) < 2 {
+		return nil, errors.New("Bad Frame")
+	}
+
+	return f[2:], nil
+}
+
+func (e *Encoding) Stop() {
+	e.stop <- true
 }
